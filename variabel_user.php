a5d89f27774d43832548aca1639df30213d15289
bilabilu123 <ininabilaa1@gmail.com>
2025-02-20 12:06:34 -1200

    praktikum 1

---
+++ variabel_user.php
@@ -0,0 +1,11 @@
+<?php
+//Mendefinisikan Variabel
+$nama = 'Nabila';
+$umur = 19;
+$berat = 42.00;
+
+echo 'Nama Saya ' .$nama;
+echo '<br/>Umur : ' .$umur . 'Tahun';
+echo '<br/>Berat : ' .$berat . 'Kg';
+
+?>
